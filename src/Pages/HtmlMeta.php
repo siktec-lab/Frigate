0f85cb7d5fce726ce7ecf91db6dfc9fab56ce44e
Shlomo Hassid <shlomohassid@gmail.com>
2022-09-13 05:44:19 +0300

    More default metas & csrf ready property

--- src/Pages/HtmlMeta.php
+++ src/Pages/HtmlMeta.php
@@ -7,11 +7,17 @@ use \Siktec\Frigate\Tools\Arrays;
 class HtmlMeta implements Arrays\ToArrayAccess {
 
     use Arrays\toArrayTrait;
-    public ?string $lang         = "en";
-    public ?string $charset      = "UTF-8";
-    public ?string $title        = "Page";
-    public ?string $description  = null;
-    public ?string $viewport     = "width=device-width, initial-scale=1.0,  shrink-to-fit=no";
-    public ?string $favicon      = null;
-
+    
+    public ?string $lang           = "en";
+    public ?string $charset        = "UTF-8";
+    public ?string $title          = "Page";
+    public ?string $description    = null;
+    public ?string $viewport       = "width=device-width, initial-scale=1.0,  shrink-to-fit=no";
+    public ?string $favicon_ico    = null;
+    public ?string $favicon_png16  = null;
+    public ?string $favicon_png32  = null;
+    public ?string $favicon_png180 = null;
+    public ?string $manifest       = null;
+    public ?string $csrf           = null;
+    public ?string $baseurl        = null;
 }
