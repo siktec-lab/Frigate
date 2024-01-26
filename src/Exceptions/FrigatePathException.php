c60856d9342f6d22e8ff1ef7938b4ca9d27bf90f
shlomohass <shlomohassid@gmail.com>
2024-01-26 15:05:41 +0200

    Removed unneeded types

--- src/Exceptions/FrigatePathException.php
+++ src/Exceptions/FrigatePathException.php
@@ -8,8 +8,8 @@ class FrigatePathException extends FrigateBaseException
 {
     public const CODE_FRIGATE_PATH_GENERAL = 420;
     public const CODE_FRIGATE_PATH_MULTIPLE_ARGS = 421;
-    public const CODE_FRIGATE_REDEFINE_PATH = 422;
-    public const CODE_FRIGATE_EXTRA_PATH_AFTER_PATH_TYPE = 423;
+    public const CODE_FRIGATE_EXTRA_PATH_AFTER_PATH_TYPE = 422;
+
     protected array $messages = [
         self::CODE_FRIGATE_PATH_GENERAL => [
             'Frigate Path general error', 0
@@ -17,12 +17,8 @@ class FrigatePathException extends FrigateBaseException
         self::CODE_FRIGATE_PATH_MULTIPLE_ARGS => [
             "Cannot have same level arguments of the same type in path : '%s'", 1
         ],
-        self::CODE_FRIGATE_REDEFINE_PATH => [
-            "Cannot redefine path : '%s'", 1
-        ],
         self::CODE_FRIGATE_EXTRA_PATH_AFTER_PATH_TYPE => [
             "Cannot have a path after a 'path' argument : '%s'", 1
-        ],
+        ]
     ];
-
 }
